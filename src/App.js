e39975ca7988a538a46f6f591dbc975a7d6a0224
Daniel Ufeli <danielufeli@yahoo.com>
2022-07-09 22:48:47 +0100

    Fix Linters

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Greeting from './components/greeting/index';
 
 const App = () => (
-  <>
+  <div className="container">
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Greeting />} />
       </Routes>
     </BrowserRouter>
-  </>
+  </div>
 );
 
 export default App;
